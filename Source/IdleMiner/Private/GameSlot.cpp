97e9a18e121a5214d88381cb67c8354712ead0ee
kayra <khayridural@gmail.com>
2024-01-05 23:55:11 +0300

    CPP Classes

---
+++ Source/IdleMiner/Private/GameSlot.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameSlot.h"
+
+// Sets default values
+AGameSlot::AGameSlot()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>("Root");
+
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	Box->SetupAttachment(RootComponent);
+	Box->SetBoxExtent(FVector(50, 50, 2));
+	Box->SetCollisionResponseToAllChannels(ECR_Block);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> DefaultSlotMesh(TEXT("/Engine/BasicShapes/Plane"));
+
+	Plane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Plane"));
+	Plane->SetupAttachment(RootComponent);
+	Plane->SetStaticMesh(DefaultSlotMesh.Object);
+}
