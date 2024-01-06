b91d6c582e1a1d8111d36b280ec02dbb0fa57b57
EfeBektas <126690737+EfeBektas@users.noreply.github.com>
2024-01-06 03:58:24 +0300

    Player movement without clamps or movement bonuses

--- Source/IdleMiner/Private/FloatingCharacterController.cpp
+++ Source/IdleMiner/Private/FloatingCharacterController.cpp
@@ -4,10 +4,20 @@
 #include "FloatingCharacterController.h"
 
 // Sets default values
-AFloatingCharacterController::AFloatingCharacterController()
+AFloatingCharacterController::AFloatingCharacterController() :
+	CameraSpeed(500.0f),
+	CameraDistance(500.0f),
+	MaxCameraDistance(500.0f),
+	MinCameraDistance(1500.0f)
+
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->AddLocalRotation(FRotator(-90.0f, 0.0f, 0.0f));
+	Camera->SetupAttachment(RootComponent);
+
 
 }
 
@@ -15,6 +25,18 @@ AFloatingCharacterController::AFloatingCharacterController()
 void AFloatingCharacterController::BeginPlay()
 {
 	Super::BeginPlay();
+
+	
+
+	GetCharacterMovement()->GravityScale = 0;
+	GetCharacterMovement()->MaxWalkSpeed = CameraSpeed;
+	SetActorLocation(FVector(0.0f, 0.0f, CameraDistance), false);
+
+	GetCharacterMovement()->MaxAcceleration = 100000.0f;
+	GetCharacterMovement()->BrakingFrictionFactor = 10000.0f;
+	GetCharacterMovement()->bUseSeparateBrakingFriction = true;
+	GetCharacterMovement()->BrakingFriction = 100000.0f;
+	GetCharacterMovement()->MaxCustomMovementSpeed = 5000;
 	
 }
 
@@ -30,5 +52,41 @@ void AFloatingCharacterController::SetupPlayerInputComponent(UInputComponent* Pl
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveNorth", this, &AFloatingCharacterController::MoveNorth);
+	PlayerInputComponent->BindAxis("MoveEast", this, &AFloatingCharacterController::MoveEast);
+
+	PlayerInputComponent->BindAxis("CloseDistance", this, &AFloatingCharacterController::ZoomIn);
+
+	PlayerInputComponent->BindAction("Haste", IE_Released, this, &AFloatingCharacterController::HastePressed);
+	PlayerInputComponent->BindAction("Haste", IE_Released, this, &AFloatingCharacterController::HasteReleased);
+}
+
+void AFloatingCharacterController::MoveNorth(float Amount)
+{
+	AddMovementInput(FVector(1, 0, 0), Amount);
+}
+
+void AFloatingCharacterController::MoveEast(float Amount)
+{
+	AddMovementInput(FVector(0, 1, 0), Amount);
+}
+
+void AFloatingCharacterController::ZoomIn(float Amount)
+{
+	SetActorLocation(GetActorLocation().operator+(FVector(0,0, Amount*50)), false);
+	
+	//FMath::Clamp(GetActorLocation().ZAxisVector, MinCameraDistance, MaxCameraDistance);
+	//FMath::Clamp(MyFloat, 0.0f, 100.0f)
+}
+
+
+void AFloatingCharacterController::HastePressed()
+{
+	CameraSpeed *= 1.5f;
+}
+
+void AFloatingCharacterController::HasteReleased()
+{
+	CameraSpeed /= 1.5f;
 }
 
--- Source/IdleMiner/Private/FloatingCharacterController.h
+++ Source/IdleMiner/Private/FloatingCharacterController.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "FloatingCharacterController.generated.h"
 
 UCLASS()
@@ -12,8 +14,26 @@ class AFloatingCharacterController : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AFloatingCharacterController();
+
+	UPROPERTY(EditAnywhere, Category = "FCC")
+	UCameraComponent* Camera;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FCC")
+	float CameraSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FCC")
+	float CameraDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FCC")
+	float MaxCameraDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FCC")
+	float MinCameraDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FCC")
+	bool HasteIsPressed;
 
 protected:
 	// Called when the game starts or when spawned
@@ -26,4 +46,13 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+
+	void MoveNorth(float Amount);
+	void MoveEast(float Amount);
+
+	void ZoomIn(float Amount);
+
+	void HastePressed();
+	void HasteReleased();
 };
